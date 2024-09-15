aef6e487478790af822beb5c24647073e1847719
Praveen <praveen16499@gmail.com>
2024-09-15 15:03:56 +0530

    This first project setup for Rest Api

---
+++ src/main/java/com/praveen/SbRestUmsApplication.java
@@ -0,0 +1,13 @@
+package com.praveen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestUmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestUmsApplication.class, args);
+	}
+
+}
